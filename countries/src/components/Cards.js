b6c6466ac19b84d783de3ee4a758598af3f1dd18
RahimliZakir <zakirer@code.edu.az>
2022-09-21 01:39:59 +0400

    Loader Added

--- countries/src/components/Cards.js
+++ countries/src/components/Cards.js
@@ -6,8 +6,10 @@ import classNames from "classnames";
 import API from "../api";
 import { addDots } from "../utils/sanitaizerUtil";
 import { Link } from "react-router-dom";
+import Loader from "./Loader";
 
 const Cards = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [countries, setCountries] = useState([]);
 
   const searchText = useSelector((state) => state.setSearchCountry);
@@ -22,10 +24,12 @@ const Cards = () => {
   // const query = `?continent=${filterData}&searchedText=${searchText}`;
 
   const getCountries = async () => {
+    setIsLoading(true);
     const { data } = await API.get("/all");
     setCountries(
       data.filter((item) => item.name.common.toLowerCase() !== "armenia")
     );
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -49,37 +53,39 @@ const Cards = () => {
     })
     ?.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-  console.log(filteredCountries);
-
   return (
     <section id="cards">
-      <Container>
-        <Row>
-          {filteredCountries?.map((item, index) => {
-            return (
-              <Col md="3" className="mb-3" key={index}>
-                <Card className={darkenClass}>
-                  <Card.Img variant="top" src={item.flags.png} />
-                  <Card.Body>
-                    <Card.Title>{item.name.common}</Card.Title>
-                    <div className="card-texts">
-                      <p>Population: {addDots(item.population)}</p>
-                      <p>Region: {item.region}</p>
-                      <p>Capital: {item.capital}</p>
-                    </div>
-                    <Link
-                      to={`details/${item.name.common.toLowerCase()}`}
-                      className="btn btn-info"
-                    >
-                      Details
-                    </Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Container>
+          <Row>
+            {filteredCountries?.map((item, index) => {
+              return (
+                <Col md="3" className="mb-3" key={index}>
+                  <Card className={darkenClass}>
+                    <Card.Img variant="top" src={item.flags.png} />
+                    <Card.Body>
+                      <Card.Title>{item.name.common}</Card.Title>
+                      <div className="card-texts">
+                        <p>Population: {addDots(item.population)}</p>
+                        <p>Region: {item.region}</p>
+                        <p>Capital: {item.capital}</p>
+                      </div>
+                      <Link
+                        to={`details/${item.name.common.toLowerCase()}`}
+                        className="btn btn-info"
+                      >
+                        Details
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      )}
     </section>
   );
 };
--- countries/src/components/Details.js
+++ countries/src/components/Details.js
@@ -6,14 +6,17 @@ import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 
 import API from "../api";
 import { addDots } from "../utils/sanitaizerUtil";
+import Loader from "./Loader";
 
 const Details = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [country, setCountry] = useState(null);
   const [borders, setBorders] = useState([]);
 
   const params = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     API.get(`/name/${params.name}`).then(({ data }) => setCountry(data[0]));
 
     // eslint-disable-next-line
@@ -26,6 +29,8 @@ const Details = () => {
       setBorders(
         data?.filter((item) => country?.borders?.includes(item?.cca3))
       );
+
+      setIsLoading(false);
     };
 
     if (country != null) getBorderCountries();
@@ -33,80 +38,84 @@ const Details = () => {
 
   return (
     <section id="country-details">
-      <Container>
-        {country && (
-          <Row>
-            <Col md="12" className="go-back-side">
-              <Link to="/" className="go-back-btn">
-                <FontAwesomeIcon icon={faArrowLeftLong} /> Back
-              </Link>
-            </Col>
-            <Col md="6" className="details-left-side">
-              <div className="img-div">
-                <img src={country.flags.png} alt="Country Flag" />
-              </div>
-            </Col>
-            <Col md="6" className="details-right-side">
-              <h3 className="country-heading">{country.name.common}</h3>
-              <div className="country-details-lists">
-                <ul className="country-details-left-list">
-                  <li>
-                    <b>Native Name: </b> {country.altSpellings[2]}
-                  </li>
-                  <li>
-                    <b>Population: </b> {addDots(country.population)}
-                  </li>
-                  <li>
-                    <b>Region: </b> {country.region}
-                  </li>
-                  <li>
-                    <b>Sub Region: </b> {country.subregion}
-                  </li>
-                  <li>
-                    <b>Capital: </b> {country.capital}
-                  </li>
-                </ul>
-                <ul className="country-details-right-list">
-                  <li>
-                    <b>Top Level Domain: </b> {country.tld}
-                  </li>
-                  <li>
-                    <b>Currencies: </b>
-                    {Object.keys(country.currencies).map((item) => (
-                      <span key={country.currencies[item].name}>
-                        {country.currencies[item].name}
-                      </span>
-                    ))}
-                  </li>
-                  <li>
-                    <b>Languages: </b>
-
-                    {Object.keys(country.languages).map((item) => {
-                      return (
-                        <span key={country.languages[item]}>
-                          {country.languages[item]}
-                        </span>
-                      );
-                    })}
-                  </li>
-                </ul>
-              </div>
-              <ul className="border-countries-ul">
-                <li>
-                  <b>Border Countries: </b>
-                </li>
-                {borders?.map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <span>{item?.name?.common}</span>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Container>
+          {country && (
+            <Row>
+              <Col md="12" className="go-back-side">
+                <Link to="/" className="go-back-btn">
+                  <FontAwesomeIcon icon={faArrowLeftLong} /> Back
+                </Link>
+              </Col>
+              <Col md="6" className="details-left-side">
+                <div className="img-div">
+                  <img src={country.flags.png} alt="Country Flag" />
+                </div>
+              </Col>
+              <Col md="6" className="details-right-side">
+                <h3 className="country-heading">{country.name.common}</h3>
+                <div className="country-details-lists">
+                  <ul className="country-details-left-list">
+                    <li>
+                      <b>Native Name: </b> {country.altSpellings[2]}
                     </li>
-                  );
-                })}
-              </ul>
-            </Col>
-          </Row>
-        )}
-      </Container>
+                    <li>
+                      <b>Population: </b> {addDots(country.population)}
+                    </li>
+                    <li>
+                      <b>Region: </b> {country.region}
+                    </li>
+                    <li>
+                      <b>Sub Region: </b> {country.subregion}
+                    </li>
+                    <li>
+                      <b>Capital: </b> {country.capital}
+                    </li>
+                  </ul>
+                  <ul className="country-details-right-list">
+                    <li>
+                      <b>Top Level Domain: </b> {country.tld}
+                    </li>
+                    <li>
+                      <b>Currencies: </b>
+                      {Object.keys(country.currencies).map((item) => (
+                        <span key={country.currencies[item].name}>
+                          {country.currencies[item].name}
+                        </span>
+                      ))}
+                    </li>
+                    <li>
+                      <b>Languages: </b>
+
+                      {Object.keys(country.languages).map((item) => {
+                        return (
+                          <span key={country.languages[item]}>
+                            {country.languages[item]}
+                          </span>
+                        );
+                      })}
+                    </li>
+                  </ul>
+                </div>
+                <ul className="border-countries-ul">
+                  <li>
+                    <b>Border Countries: </b>
+                  </li>
+                  {borders?.map((item, index) => {
+                    return (
+                      <li key={index}>
+                        <span>{item?.name?.common}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </Col>
+            </Row>
+          )}
+        </Container>
+      )}
     </section>
   );
 };
